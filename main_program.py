7fac7cc0437e2bdb4ff7b1e104bdb8f98daa4b1a
Saurrabhh <saurabhkyadav99@gmail.com>
2022-05-14 22:37:21 +0530

    found image

--- main_program.py
+++ main_program.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
 url = "https://w13.mangafreak.net/"
 result = requests.get(url).text
 
@@ -9,17 +10,19 @@ doc = BeautifulSoup(result, "html.parser")
 
 # print(doc.prettify())
 
-m_list = doc.find_all(class_="latest_item")
-print(m_list[0].find())
+mangas = doc.find_all(class_="latest_item")
+# print(mangas)
+
 
 main_list = []
 
-for i in range(len(m_list)):
-    name = m_list[i].find(class_="name").string
+for manga in mangas:
+    name = manga.find(class_="name").string
     # print(name)
     main_list.append(name)
 
-    # image = m_list[i].find(class_="image")
-    # print(m_list[i].get('src'))
+    image = manga.find(class_="image").img
+    print(image['src'])
+
 
 # print(main_list)
